edb5834aa37874755f05a2e3d8e7c69f317415d8
Xabi <xabierguitianlopez@gmail.com>
2024-11-10 23:00:12 +0100

    Design an interface
    developer.apple.com/tutorials/develop-in-swift/design-an-interface

---
+++ OnboardingFlow/OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Xabi on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnboardingFlow/OnboardingFlowTests/OnboardingFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowTests.swift
+//  OnboardingFlowTests
+//
+//  Created by Xabi on 10/11/24.
+//
+
+import Testing
+@testable import OnboardingFlow
+
+struct OnboardingFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
