fe1c940e4aea3395f7450da61e6b77105eca6a08
x.guitian <x.guitian@udc.es>
2025-02-08 17:02:22 +0100

    Create dynamic Content
    developer.apple.com/tutorials/develop-in-swift/create-dynamic-content

---
+++ Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Xabi on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
