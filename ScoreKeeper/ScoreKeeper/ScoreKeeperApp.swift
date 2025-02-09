a329e3436a08a22efed9507a7fd98053103a06bd
x.guitian <x.guitian@udc.es>
2025-02-09 21:29:21 +0100

    Model data with custom types
    https://developer.apple.com/tutorials/develop-in-swift/model-data-with-custom-types/

---
+++ ScoreKeeper/ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Xabi on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
